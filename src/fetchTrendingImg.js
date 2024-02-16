4960c31861b74d0b645dc91b6e373246d664ef85
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-02-16 10:10:37 +0100

    Update fetchTrendingImg.js

--- src/fetchTrendingImg.js
+++ src/fetchTrendingImg.js
@@ -4,9 +4,8 @@ export const FetchTrendingImg = async (movie_id, controller) => {
   const myApiKey =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDZjMDU2M2ZkYWIyNDA2MzM0ZWI0NjZhNjdhNmViNCIsInN1YiI6IjY1YzQ5ZjgwMDdmYWEyMDE3ZGMyYTEzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yQ30NlTA65GVVPMGbKblQ2KwUXBxE-TuJESB3plLuFE";
   const options = {
+    // Замість my_unique_api_key вставте свій ключ
     headers: {
-      // Замість my_unique_api_key вставте свій ключ
-
       Authorization: `Bearer ${myApiKey}`,
     },
     signal: controller.signal,
